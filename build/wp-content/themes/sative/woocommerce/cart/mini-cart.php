8171be8d3fda0db2020963678f67b76781bb329b
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2018-03-27 23:32:20 +0100

    testing

--- build/wp-content/themes/sative/woocommerce/cart/mini-cart.php
+++ build/wp-content/themes/sative/woocommerce/cart/mini-cart.php
@@ -45,8 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						$product_id   = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
 						if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-							$product_name      = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key );
-							$thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+							$thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image('thumbnail'), $cart_item, $cart_item_key );
 							$product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 							$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 							?>
@@ -62,7 +61,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 								), $cart_item_key );
 								?>
 								<div class="photo">
-									<?= $thumbnail; ?>
+									<?= $_product->get_image('thumbnail'); ?>
 								</div>
 								<div class="info">
 									<div class="info__title">
