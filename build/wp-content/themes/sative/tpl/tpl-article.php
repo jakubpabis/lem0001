34dfcc1a00ac1dd21be51ae36ac5aa7b1aaf58bd
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2018-05-12 14:12:06 +0200

    add soem

---
+++ build/wp-content/themes/sative/tpl/tpl-article.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Generic article */ ?>
+<?php 
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header(); ?>
+
+<main class="generic article">
+    <div class="container container-xsml">
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
+            <?php the_content(); ?>
+
+        <?php endwhile; // end of the loop. ?>
+    </div>
+</main>  
+
+<?php get_footer(); ?>
