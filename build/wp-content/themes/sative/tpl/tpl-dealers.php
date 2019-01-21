29fbaa55624829e536e95221dfd02282e0f43df0
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2019-01-21 21:46:09 +0100

    make maps at dealers

--- build/wp-content/themes/sative/tpl/tpl-dealers.php
+++ build/wp-content/themes/sative/tpl/tpl-dealers.php
@@ -17,6 +17,8 @@ get_header(); ?>
             </div>
         </div>
 
+        <div class="dealers__map" id="dealers_map"></div>
+
         <div class="dealers__grid container container-sml">
 
             <?php while ( have_rows('dealers') ) : the_row(); ?>
@@ -45,5 +47,57 @@ get_header(); ?>
 
     <?php endwhile; // end of the loop. ?>
 </section>
+<script>
+    function initMap() {
 
+        var markers = [
+
+            <?php while ( have_rows('dealers') ) : the_row(); ?>
+                <?php $name = get_sub_field('title'); ?>
+                <?php $lat = floatval(get_sub_field('latitude')); ?>
+                <?php $lng = floatval(get_sub_field('longitude')); ?>
+                <?php $index = get_row_index(); ?>
+
+                <?php if(!empty($lat) && !empty($lng)) : ?>
+                    ['<?= $name; ?>', <?= $lat; ?>, <?= $lng; ?>, <?= $index; ?>],
+                <?php endif; ?>
+
+            <?php endwhile; ?>
+
+        ];
+
+        // Create a map object and specify the DOM element
+        // for display.
+        var map = new google.maps.Map(document.getElementById('dealers_map'), {
+            zoom: 6,
+            center: new google.maps.LatLng(markers[0][1], markers[0][2])
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        for (i = 0; i < markers.length; i++) {  
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+
+        var bounds = new google.maps.LatLngBounds();
+        for (var j = 0; j < markers.length; j++) {
+            bounds.extend(new google.maps.LatLng(markers[j][1], markers[j][2]));
+        }
+        // Create a marker and set its position.
+        map.fitBounds(bounds);
+
+    }
+
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChUaXItSbUlby58mbo2-8VEG07BMKFphI&callback=initMap" async defer></script>
 <?php get_footer(); ?>
