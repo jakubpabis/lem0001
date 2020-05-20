16de1b4e890cc9ede51f3dcae4141c8849d11c06
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-05-20 09:50:45 +0200

    changes to slider

--- prototype/dist/js/main.js
+++ prototype/dist/js/main.js
@@ -282,6 +282,9 @@ function initOwlCarousel()
 		nav: false,
 		dots: false,
 		speed: 500,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
 		responsive: {
 			0: {
 				items: 1
